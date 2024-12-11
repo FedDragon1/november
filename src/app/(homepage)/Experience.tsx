1d72c04fa925c2a43fc10e0d2e43fc693376473a
fedel <fedelewu@outlook.com>
2024-12-10 20:25:18 -0800

    experience page

--- src/app/(homepage)/Experience.tsx
+++ src/app/(homepage)/Experience.tsx
@@ -1,11 +1,43 @@
-import { FC } from "react";
+import React, { FC } from "react";
+import FancyButton from "@/app/FancyButton";
 
 interface Props {}
 
 const Experience: FC<Props> = () => {
     return (
-        <section className="w-[200vw] h-full border-2 border-yellow-500 flex-shrink-0">
-            Experiences
+        <section className="w-[2600px] h-full relative flex-shrink-0 pl-80 bg-midground">
+            <div className="flex flex-col gap-5">
+                <div className="h-[22rem] z-20 relative mix-blend-exclusion">
+                    <h1 className="text-[12rem] mt-24 text-white">An unforgettable memory</h1>
+                </div>
+                <span className="uppercase">Some subtitle</span>
+                <div className="flex gap-10 flex-col max-w-[1200px]">
+                    <div className="flex-1">
+                        <p className="text-6xl">Beatae error iure, magnam officia optio sunt voluptates. Ad, animi autem
+                            debitis eius
+                            praesentium quibusdam similique.</p>
+                    </div>
+                    <hr className="border-zinc-500"/>
+                    <div>
+                        <span className="uppercase">Some subtitle</span>
+                        <div className="flex gap-10 max-w-[800px]">
+                            <p className="flex-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+                                delectus excepturi iure necessitatibus, possimus reprehenderit vel voluptas!</p>
+                            <div className="flex-1">
+                                <p>Beatae error iure, magnam officia optio sunt voluptates. Ad, animi autem debitis eius
+                                    praesentium quibusdam similique voluptatum. At cumque error quas unde.</p>
+                            </div>
+                        </div>
+                        <FancyButton>Learn More</FancyButton>
+                    </div>
+                </div>
+            </div>
+            <div className="absolute right-0 top-0 -z-0 grayscale">
+                <img src="https://picsum.photos/800/1080" alt="picture" />
+                <div className="absolute bottom-0 left-[-230px]">
+                    <img src="https://picsum.photos/260/500" alt="picture"/>
+                </div>
+            </div>
         </section>
     )
 }
--- src/app/FancyButton.tsx
+++ src/app/FancyButton.tsx
@@ -11,7 +11,7 @@ const FancyButton: FC<FancyButtonProps> = ({ className, children }) => {
     return (
         <button className={`group w-40 h-16 relative overflow-hidden border-b-black cursor-pointer fancy-button border-b-2 
                 my-5 hover:text-background transition-all ease-in ${className ?? ''}`}>
-            <div className="flex gap-1 justify-center items-center">
+            <div className="flex gap-1 justify-center items-center relative z-20">
                 { children }
                 <GoArrowUpRight size="1rem" className="group-hover:translate-x-2 transition-all" />
             </div>
